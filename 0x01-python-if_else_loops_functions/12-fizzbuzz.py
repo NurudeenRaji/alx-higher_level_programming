86afd6aedab98721703052ee469b0eaca367d5d1
Raji Nurudeen <orizon.bot21@gmail.com>
2023-12-06 13:15:19 +0100

    if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        three = i % 3
+        five = i % 5
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif three == 0:
+            print("Fizz", end=' ')
+        elif five == 0:
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
+    print("$", end='')
