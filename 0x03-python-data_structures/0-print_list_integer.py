1127a6f5fb671f0296179241ed6553a727955d85
Raji Nurudeen <orizon.bot21@gmail.com>
2023-12-09 21:18:14 +0100

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    size = len(my_list)
-    for i in range(0, size):
-        print("{}".format(my_list[i]))
+if __name__ = "__main__":
+    def print_list_integer(my_list=[]):
+        size = len(my_list)
+        for i in range(0, size):
+            print("{}".format(my_list[i]))
