e2ae8d4039b6f3c81f2718daed2674635b2b8ebc
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-04 11:48:46 +0100

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This is a  script to create a Square class """
+
+
+class Square:
+    """ The square class with a private instance attribute """
+
+    def __init__(self, size=0):
+        """ Constructor initialized with arguments.
+
+        size : int
+            A private size of the square
+        """
+        self.__size = size
+
+        try:
+            if type(self.__size) is not int:
+                raise TypeError
+            elif self.__size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
