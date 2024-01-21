f67542e49c45d6c4ccd6792343da616406618962
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-21 18:30:27 +0100

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+"""
+A function to print my name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Function to print first_name & last_name
+
+    first_name: first param
+    last_name: Second param
+    return: nothing.
+    """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
