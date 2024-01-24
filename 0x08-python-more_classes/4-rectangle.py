c0f6de37499821693b02e6038cea77061ef5550f
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-24 16:43:41 +0100

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Rectangle class module
+"""
+
+
+class Rectangle:
+    """
+    This is a rectangle class with width and height attributes
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        area = self.width * self.height
+        return area
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            perimeter = 0
+        else:
+            perimeter = 2 * (self.width + self.height)
+        return perimeter
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            result = ""
+            for i in range(self.height):
+                if i != 0:
+                    result += "\n"
+                for j in range(self.width):
+                    result += "#"
+            return result
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
