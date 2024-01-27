459d2e68e118b7304c174bcd045c4092734b0cd6
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-27 14:15:15 +0100

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,6 @@ This module has only one function.
 def add_integer(a, b=98):
     """
     A function adding two integers
-
-    param a: the first integer
-    param b: has default value of 98.
-    return: the sum of the two parameter.
     """
 
     if not (type(a) is int or type(a) is float):
