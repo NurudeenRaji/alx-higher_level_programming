23bc09f6bb24b10589418c9515af4d18b797f941
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-28 21:14:46 +0100

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python inheritance """
+
+
+def lookup(obj):
+    """
+    A function that returns the list of available attributes
+    and methods of an object
+    """
+
+    return dir(obj)
