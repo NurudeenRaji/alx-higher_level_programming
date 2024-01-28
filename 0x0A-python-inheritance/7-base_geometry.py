e0ae47995a2e38c774bfc3fdac198583ab749698
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-28 23:50:14 +0100

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,9 +12,17 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         self.name = name
         self.value = value
+
         if type(self.name) is not str:
             raise TypeError("name must be a string")
+        if self.name is None or self.name is bool:
+            raise TypeError("name must be a string")
+        if len(self.name) == 0:
+            raise TypeError("name must not be an empty string")
+
         if type(self.value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value is None or self.value is bool:
             raise TypeError("{} must be an integer".format(self.name))
         if self.value <= 0:
             raise ValueError("{} must be greater than 0".format(self.name))
