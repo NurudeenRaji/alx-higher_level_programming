ce06350c1167e8ce3f052631d38c26832e4f3a16
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-28 22:11:18 +0100

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,4 +10,11 @@ class MyList(list):
     def print_sorted(self):
         """ A method tht prints the list in sorted form """
 
+        if not isinstance(self, list):
+            raise ValueError("List expected.")
+        if None in self:
+            raise ValueError("List elements must be numbers")
+        if not all(isinstance(x, int) for x in self):
+            raise ValueError("List must be a list of numbers.")
+
         print(sorted(self))
