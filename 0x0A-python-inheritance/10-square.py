b0981f11243dadeeba8dac014f330ab78f0afd54
Raji Nurudeen <orizon.bot21@gmail.com>
2024-01-29 22:55:21 +0100

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -64,4 +64,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return a string representation of the square."""
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return ("[Square] {}/{}".format(self.__size, self.__size))
