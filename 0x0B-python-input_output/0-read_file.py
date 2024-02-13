62503004c56ec2189d970ed717b0938b003dedfc
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-13 18:40:24 +0100

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 
 def read_file(filename=""):
     """ Function that reads a text file and prints it to stdout """
-    
+
     with open(filename, encoding='UTF-8') as file:
         for a in file:
             print("{}".format(a), end="")
