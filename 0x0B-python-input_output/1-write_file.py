9a77bb69ccd8c8ebd9d485ba88b54593b68ba1fa
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-13 22:00:02 +0100

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" module 1. Write to a file """
+
 
 def write_file(filename="", text=""):
+    """ function that writes a string to a text file
+        and returns the number of characters """
+
     with open(filename, mode='w', encoding='UTF_8') as file:
         new = file.write(text)
         return new
