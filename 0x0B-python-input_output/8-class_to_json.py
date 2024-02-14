b5e30818ee546b0506a12e0da7af0ee80210a762
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-14 23:23:42 +0100

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module 8. Class to JSON """
+import json
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary description
+        with simple data structure
+    """
+
+    return obj.__dict__
