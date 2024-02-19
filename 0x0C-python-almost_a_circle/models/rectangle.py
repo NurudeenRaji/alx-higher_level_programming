f7f84d08a8efac9c1477157c6d9e9df8e42f014a
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-19 23:09:53 +0100

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,9 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """ Public method to override the __str__ method """
+
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                self.__y, self.__width, self.__height))
