7d18d3159a076edfd03508ffe4398a980429ab8a
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-19 22:05:50 +0100

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, val):
         """ setter for private class attributes  - width """
+        if not isinstance(val, int):
+            raise TypeError("width must be a integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
         self.__width = val
 
     @property
@@ -26,6 +30,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, val):
         """ setter for private class attributes  - height """
+        if not isinstance(val, int):
+            raise TypeError("height must be a integer")
+        if val <= 0:
+            raise ValueError("height must be > 0")
         self.__height = val
 
     @property
@@ -36,6 +44,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, val):
         """ setter for private class attributes  - x """
+        if not isinstance(val, int):
+            raise TypeError("x must be a integer")
+        if val < 0:
+            raise ValueError("x must be >= 0")
         self.__x = val
 
     @property
@@ -46,10 +58,30 @@ class Rectangle(Base):
     @y.setter
     def y(self, val):
         """ setter for private class attributes  - y """
+        if not isinstance(val, int):
+            raise TypeError("y must be a integer")
+        if val < 0:
+            raise ValueError("y must be >= 0")
         self.__y = val
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Constructor to initiate the class and inherited attributes """
+        if not isinstance(width, int):
+            raise TypeError("width must be a integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be a integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if not isinstance(x, int):
+            raise TypeError("x must be a integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if not isinstance(y, int):
+            raise TypeError("y must be a integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         super().__init__(id)
         self.__width = width
         self.__height = height
