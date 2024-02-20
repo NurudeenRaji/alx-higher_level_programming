d5847f70681237a85c710274e37117f0ae2609fc
Raji Nurudeen <orizon.bot21@gmail.com>
2024-02-20 15:36:52 +0100

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,27 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Square cass that inherits from the class Rectangle """
 
+    @property
+    def size(self):
+        """ getter method for the square size """
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """ setter method for the square size """
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
+        self.width = val
+        self.height = val
+
     def __init__(self, size, x=0, y=0, id=None):
         """ Constructor for the square class """
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
